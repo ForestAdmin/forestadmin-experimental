8115020fd71390f318f6ab0c26a4b52c497b49be
steveb <bunlon.steve@gmail.com>
2023-10-05 15:51:53 +0200

    fix(gcs-client): refresh gcs client to support account rollout

--- packages/plugin-gcs/src/utils/gcs.ts
+++ packages/plugin-gcs/src/utils/gcs.ts
@@ -3,20 +3,23 @@ import { Bucket, Storage } from '@google-cloud/storage';
 import { File, Options } from '../types';
 
 export default class Client {
-  private client: Bucket;
+  private options: Options['gcs'];
 
   constructor(options: Options['gcs']) {
     if (!options.bucketId || !options.projectId) {
       throw new Error('Your gcs configuration is incorrect, missing either bucketId or projectId.');
     }
 
-    this.client = (new Storage({
-      keyFile: options.keyFilePath,
-      projectId: options.projectId,
-      keyFilename: options.keyFilePath,
-    })).bucket(options.bucketId)
+    this.options = options;
   }
 
+  private get client() {
+    return (new Storage({
+      keyFile: this.options.keyFilePath,
+      projectId: this.options.projectId,
+      keyFilename: this.options.keyFilePath,
+    })).bucket(this.options.bucketId)
+  }
   async getSignedUrl(key: string): Promise<string> {
     const response = await this.client.file(key).getSignedUrl({
       expires: Date.now() + 1000 * 60 * 60,
