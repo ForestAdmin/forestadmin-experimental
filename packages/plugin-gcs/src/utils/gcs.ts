f31b8cf7a1b1522c09e4c26083e57c52d4e4af24
Arnaud Moncel <arnaud.moncel43@gmail.com>
2023-10-05 15:36:04 +0200

    chore(pluggin gcs): improve log and error

--- packages/plugin-gcs/src/utils/gcs.ts
+++ packages/plugin-gcs/src/utils/gcs.ts
@@ -1,48 +1,71 @@
+import { Logger } from '@forestadmin/datasource-toolkit';
 import { Bucket, Storage } from '@google-cloud/storage';
 
 import { File, Options } from '../types';
+import GCSClientError from '../error';
 
 export default class Client {
   private options: Options['gcs'];
+  private logger: Logger;
 
-  constructor(options: Options['gcs']) {
+  constructor(options: Options['gcs'], logger: Logger) {
     if (!options.bucketId || !options.projectId) {
       throw new Error('Your gcs configuration is incorrect, missing either bucketId or projectId.');
     }
 
     this.options = options;
+    this.logger = logger;
   }
 
-  private get client() {
+  private get client(): Bucket {
     return (new Storage({
       keyFile: this.options.keyFilePath,
       projectId: this.options.projectId,
       keyFilename: this.options.keyFilePath,
-    })).bucket(this.options.bucketId)
+    })).bucket(this.options.bucketId);
   }
+
   async getSignedUrl(key: string): Promise<string> {
-    const response = await this.client.file(key).getSignedUrl({
-      expires: Date.now() + 1000 * 60 * 60,
-      action: 'read',
-    });
+    try {
+      this.logger('Debug', `Get signed url for ${key}`);
 
-    return response[0];
+      const response = await this.client.file(key).getSignedUrl({
+        expires: Date.now() + 1000 * 60 * 60,
+        action: 'read',
+      });
+
+      return response[0];
+    } catch (error) {
+      throw new GCSClientError(error.message);
+    }
   }
 
-  async load(Key: string): Promise<File> {
-    const response = await this.client.file(Key).download();
-    const name = Key.substring(Key.lastIndexOf('/') + 1);
+  async load(key: string): Promise<File> {
+    try {
+      this.logger('Debug', `Download ${key} file`);
 
-    return {
-      buffer: response[0],
-      mimeType: '',
-      name,
-    };
+      const response = await this.client.file(key).download();
+      const name = key.substring(key.lastIndexOf('/') + 1);
+
+      return {
+        buffer: response[0],
+        mimeType: '',
+        name,
+      };
+    } catch (error) {
+      throw new GCSClientError(error.message);
+    }
   }
 
-  async save(Key: string, file: File): Promise<void> {
-    await this.client.file(Key).save(file.buffer, {
-      contentType: file.mimeType,
-    });
+  async save(key: string, file: File): Promise<void> {
+    try {
+      this.logger('Debug', `Upload ${key} file`);
+
+      await this.client.file(key).save(file.buffer, {
+        contentType: file.mimeType,
+      });
+    } catch (error) {
+      throw new GCSClientError(error.message);
+    }
   }
 }
