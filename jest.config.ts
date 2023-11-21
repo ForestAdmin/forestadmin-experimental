579f6b4254db914855b2d7e5f90da162b3d78142
Jeff LADIRAY <ladirayjeff@gmail.com>
2023-11-21 11:32:39 +0100

    ci: add ci/cd

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    '<rootDir>/packages/*/src/**/*.ts',
+  ],
+  testMatch: ['<rootDir>/packages/*/test/**/*.test.ts'],
+  setupFilesAfterEnv: ['jest-extended/all'],
+};
+export default config;
