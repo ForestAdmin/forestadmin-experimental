682b18ec33fc317f6a81b71c75194d1a249c98a6
Jeff LADIRAY <ladirayjeff@gmail.com>
2023-11-21 12:31:23 +0100

    fix: update live-demo-blocker (#25)

--- packages/live-demo-blocker/src/index.ts
+++ packages/live-demo-blocker/src/index.ts
@@ -9,8 +9,8 @@ import ResultBuilder from '@forestadmin/datasource-customizer/dist/decorators/ac
 import { ActionResult } from '@forestadmin/datasource-toolkit';
 
 export type LiveDemoBlockerOptions = {
-  userEmail?: string;
-  errorMessage?: string;
+  userEmail?: string; // The user email to use
+  errorMessage?: string; // The error message to display
 };
 
 const LIVE_DEMO_USER_EMAIL_DEFAULT = 'erlich.bachman@forestadmin.com';
