06d0ddaccfa5392418631293ef7cd0048acae763
Thenkei <morganperre@gmail.com>
2023-12-05 10:28:25 +0100

    test(elasticsearch): add aggregations integrations tests on date operations aggregations

--- packages/datasource-elasticsearch/test/crud/aggregate.integration.test.ts
+++ packages/datasource-elasticsearch/test/crud/aggregate.integration.test.ts
@@ -10,26 +10,32 @@ const dataset = [
   {
     discriminant: 'blue',
     weight: 1,
+    date: '2023-11-23',
   },
   {
     discriminant: 'blue',
     weight: 21,
+    date: '2023-11-21',
   },
   {
     discriminant: 'blue',
     weight: 3,
+    date: '2023-10-21',
   },
   {
     discriminant: 'red',
     weight: 4,
+    date: '2023-06-21',
   },
   {
     discriminant: 'red',
     weight: 5,
+    date: '2023-01-21',
   },
   {
     discriminant: 'red',
     weight: 6,
+    date: '2022-01-01',
   },
 ];
 
@@ -43,7 +49,7 @@ afterAll(async () => {
   await deleteElasticsearchIndex(indexName);
 });
 
-describe('Collection > CRUD > update', () => {
+describe('Collection > CRUD > aggregate', () => {
   describe('Count', () => {
     it('should count record record', async () => {
       const collection = await getCollectionForIndex(indexName);
@@ -265,6 +271,98 @@ describe('Collection > CRUD > update', () => {
       );
 
       expect(result[0]).toMatchObject(expect.objectContaining({ value: 4 }));
+    });
+  });
+
+  describe('Date Operations', () => {
+    it.each([
+      [
+        'Year',
+        [
+          { group: { date: '2022-01-01' }, value: 1 },
+          { group: { date: '2023-01-01' }, value: 5 },
+        ],
+      ],
+      [
+        'Month',
+        [
+          { group: { date: '2022-01-01' }, value: 1 },
+          { group: { date: '2022-02-01' }, value: 0 },
+          { group: { date: '2022-03-01' }, value: 0 },
+          { group: { date: '2022-04-01' }, value: 0 },
+          { group: { date: '2022-05-01' }, value: 0 },
+          { group: { date: '2022-06-01' }, value: 0 },
+          { group: { date: '2022-07-01' }, value: 0 },
+          { group: { date: '2022-08-01' }, value: 0 },
+          { group: { date: '2022-09-01' }, value: 0 },
+          { group: { date: '2022-10-01' }, value: 0 },
+          { group: { date: '2022-11-01' }, value: 0 },
+          { group: { date: '2022-12-01' }, value: 0 },
+          { group: { date: '2023-01-01' }, value: 1 },
+          { group: { date: '2023-02-01' }, value: 0 },
+          { group: { date: '2023-03-01' }, value: 0 },
+          { group: { date: '2023-04-01' }, value: 0 },
+          { group: { date: '2023-05-01' }, value: 0 },
+          { group: { date: '2023-06-01' }, value: 1 },
+          { group: { date: '2023-07-01' }, value: 0 },
+          { group: { date: '2023-08-01' }, value: 0 },
+          { group: { date: '2023-09-01' }, value: 0 },
+          { group: { date: '2023-10-01' }, value: 1 },
+          { group: { date: '2023-11-01' }, value: 2 },
+        ],
+      ],
+      [
+        'Week',
+        [
+          { group: { date: '2021-12-27' }, value: 1 },
+          { group: { date: '2022-01-03' }, value: 0 },
+          { group: { date: '2022-01-10' }, value: 0 },
+          { group: { date: '2022-01-17' }, value: 0 },
+          // ...
+          { value: 1, group: { date: '2023-01-16' } },
+          { value: 0, group: { date: '2023-01-23' } },
+          // ...
+          { value: 0, group: { date: '2023-05-29' } },
+          { value: 0, group: { date: '2023-06-05' } },
+          { value: 0, group: { date: '2023-06-12' } },
+          { value: 1, group: { date: '2023-06-19' } },
+          { value: 0, group: { date: '2023-06-26' } },
+          { value: 0, group: { date: '2023-07-03' } },
+          // ...
+          { value: 0, group: { date: '2023-10-02' } },
+          { value: 0, group: { date: '2023-10-09' } },
+          { value: 1, group: { date: '2023-10-16' } },
+          { value: 0, group: { date: '2023-10-23' } },
+          { value: 0, group: { date: '2023-10-30' } },
+          { value: 0, group: { date: '2023-11-06' } },
+          { value: 0, group: { date: '2023-11-13' } },
+          { value: 2, group: { date: '2023-11-20' } },
+        ],
+      ],
+      [
+        'Day',
+        [
+          { group: { date: '2022-01-01' }, value: 1 },
+          { group: { date: '2023-06-03' }, value: 0 },
+          { group: { date: '2023-06-21' }, value: 1 },
+          { group: { date: '2023-10-21' }, value: 1 },
+          { group: { date: '2023-11-21' }, value: 1 },
+          { group: { date: '2023-11-23' }, value: 1 },
+        ],
+      ],
+    ])('should create bucket aggregation that count by "%s"', async (dateOperation, expected) => {
+      const collection = await getCollectionForIndex(indexName);
+
+      const result = await collection.aggregate(
+        null as unknown as Caller,
+        {} as unknown as Filter,
+        {
+          operation: 'Count',
+          groups: [{ field: 'date', operation: dateOperation }],
+        } as unknown as Aggregation,
+      );
+
+      expect(result).toMatchObject(expect.arrayContaining(expected));
     });
   });
 });
