e45a7b5eab3d530212e418451c6c5340fb9c89a8
Guillaume Gautreau <guillaume+github@ghusse.com>
2024-02-08 08:27:27 +0100

    feat: allow to add tests on search (#59)

--- packages/agent-nodejs-testing/src/integrations/types.ts
+++ packages/agent-nodejs-testing/src/integrations/types.ts
@@ -4,6 +4,7 @@ import { PlainFilter, PlainSortClause } from '@forestadmin/datasource-toolkit';
 export type TestableBaseOptions = {
   filters?: PlainFilter;
   sort?: PlainSortClause;
+  search?: string;
 };
 
 export type TestableAgentOptions = AgentOptions & { port?: number };
