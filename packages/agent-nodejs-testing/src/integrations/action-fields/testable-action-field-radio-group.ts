0ae9217ff6caa0c13812f6ba9be58081d1992d5c
scra <albanbertolini@gmail.com>
2024-03-29 15:42:53 +0100

    fix(testing): fix the select in the dropdown (#69)
    Co-authored-by: alban bertolini <albanbertolini@MacBook-Pro-de-alban.local>

--- packages/agent-nodejs-testing/src/integrations/action-fields/testable-action-field-radio-group.ts
+++ packages/agent-nodejs-testing/src/integrations/action-fields/testable-action-field-radio-group.ts
@@ -1,8 +1,13 @@
 import TestableActionField from './testable-action-field';
+import { PlainFieldOption } from './types';
 
 export default class TestableActionFieldRadioGroup<
   TypingsSchema,
 > extends TestableActionField<TypingsSchema> {
+  async getOptions(): Promise<PlainFieldOption[] | undefined> {
+    return this.fieldsFormStates.getMultipleChoiceField(this.name).getOptions();
+  }
+
   async check(option: string): Promise<void> {
     const opt = this.fieldsFormStates.getMultipleChoiceField(this.name).getOption(option);
     await this.fieldsFormStates.setFieldValue(this.name, opt.value);
