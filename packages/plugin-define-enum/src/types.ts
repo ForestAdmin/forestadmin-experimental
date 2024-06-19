06772f7bee2e424ca53a48f52ccff5867a5af416
Thenkei <morganperre@gmail.com>
2024-06-19 11:35:04 +0200

    fix: review

--- packages/plugin-define-enum/src/types.ts
+++ packages/plugin-define-enum/src/types.ts
@@ -1,4 +1,4 @@
-import type { TCollectionName, TFieldName, TSchema } from '@forestadmin/datasource-customizer';
+import type { TCollectionName, TColumnName, TSchema } from '@forestadmin/datasource-customizer';
 
 /**
  * Configuration for the GCS bucket addon of Forest Admin.
@@ -9,7 +9,7 @@ export type Options<
   N extends TCollectionName<S> = TCollectionName<S>,
 > = {
   /** Target field to turn into an enum, for example a status stored as a number */
-  fieldName: TFieldName<S, N>; // or TColumnName
+  fieldName: TColumnName<S, N>;
 
   /** The object with enum descriptors as keys, and real values as... values. */
   enumObject: Record<string, unknown>;
