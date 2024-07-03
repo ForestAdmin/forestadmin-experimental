2a82a996985c573fc015a2291fa7446926bc980f
Arnaud Moncel <arnaud.moncel43@gmail.com>
2024-07-03 15:43:17 +0200

    chore: remove useless files

--- packages/rpc-agent/test/__factories__/agent.ts
+++
@@ -1,6 +0,0 @@
-import { Factory } from 'fishery';
-
-import forestAdminHttpDriverOptions from './forest-admin-http-driver-options';
-import Agent from '../../src/agent';
-
-export default Factory.define<Agent>(() => new Agent(forestAdminHttpDriverOptions.build()));
--- packages/rpc-agent/test/__factories__/serializer.ts
+++
@@ -1,5 +0,0 @@
-import { Factory } from 'fishery';
-
-import Serializer from '../../src/services/serializer';
-
-export default Factory.define<Serializer>(() => new Serializer());
