b3f24f637b6c56b0805fb5f46293e4a0c1c76336
Thenkei <morganperre@gmail.com>
2024-09-06 14:00:07 +0200

    feat(elasticsearch): release support of v8 and drop support of v7

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverageFrom: [
-    '<rootDir>/packages/*/src/**/*.ts',
-  ],
+  collectCoverageFrom: ['<rootDir>/packages/*/src/**/*.ts'],
   testMatch: ['<rootDir>/packages/*/test/**/*.test.ts'],
   setupFilesAfterEnv: ['jest-extended/all'],
 };
--- packages/datasource-elasticsearch/jest.config.ts
+++ packages/datasource-elasticsearch/jest.config.ts
@@ -1,10 +1,14 @@
-/* eslint-disable import/no-relative-packages */
+import type { Config } from '@jest/types';
+
+// eslint-disable-next-line import/no-relative-packages
 import jestConfig from '../../jest.config';
 
-export default {
+const config: Config.InitialOptions = {
   ...jestConfig,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testMatch: ['<rootDir>/test/**/*.test.ts'],
   testTimeout: 10_000,
   maxWorkers: 1,
 };
+
+export default config;
