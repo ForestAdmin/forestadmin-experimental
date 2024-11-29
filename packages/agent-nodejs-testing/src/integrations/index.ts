150ce7498b4d5087d95b66e44afe983717e0d710
scra <albanbertolini@gmail.com>
2024-11-29 16:34:36 +0100

    feat: improve the readme and improve function naming (#122)

--- packages/agent-nodejs-testing/src/integrations/index.ts
+++ packages/agent-nodejs-testing/src/integrations/index.ts
@@ -14,11 +14,11 @@ export { AgentOptions, Agent } from '@forestadmin/agent';
 export * from './types';
 
 export { SchemaPathManager, ForestServerSandbox, TestableAgent };
-export type ForestClient = TestableAgentBase;
+export type ForestAgentClient = TestableAgentBase;
 
 /**
  * Create a forest server sandbox
- * It is useful to test the agent if you use the createForestClient way to test your agent
+ * It is useful to test the agent if you use the createForestAgentClient way to test your agent
  * @param port
  */
 export async function createForestServerSandbox(port: number): Promise<ForestServerSandbox> {
@@ -32,13 +32,13 @@ export async function createForestServerSandbox(port: number): Promise<ForestSer
  * You can test any agent with this client (python, ruby, nodeJs, etc.)
  * @param options
  */
-export async function createForestClient(options: {
+export async function createForestAgentClient(options: {
   agentForestEnvSecret: string;
   agentForestAuthSecret: string;
   agentUrl: string;
   serverUrl: string;
   agentSchemaPath: string;
-}): Promise<ForestClient> {
+}): Promise<ForestAgentClient> {
   const { serverUrl, agentForestAuthSecret, agentUrl, agentSchemaPath } = options;
   let schema: ForestSchema;
 
@@ -64,8 +64,8 @@ export async function createForestClient(options: {
  * Create a testable agent
  * You can test your agentNodejs customizations by injecting your customizations.
  * It will start the agent for you. You don't need to start the agent by yourself and a server.
- * It's not compatible with the createForestClient & createForestServerSandbox way.
- * It is recommended to user createForestClient & createForestServerSandbox to test your agent.
+ * It's not compatible with the createForestAgentClient & createForestServerSandbox way.
+ * It is recommended to user createForestAgentClient & createForestServerSandbox to test your agent.
  *
  * @param customizer
  * @param options
