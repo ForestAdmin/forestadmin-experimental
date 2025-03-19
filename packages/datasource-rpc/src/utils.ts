ce38a5c6e69d60be2ffdd4ed29491536e0e9d865
Arnaud Moncel <arnaud.moncel43@gmail.com>
2025-03-19 10:20:28 +0100

    chore: fix lint

--- packages/datasource-rpc/src/utils.ts
+++ packages/datasource-rpc/src/utils.ts
@@ -1,5 +1,6 @@
 import { createHmac } from 'crypto';
 
+// eslint-disable-next-line import/prefer-default-export
 export function setAuth(req, authSecret: string) {
   const timeStamp = new Date().toUTCString();
   const token = createHmac('sha256', authSecret).update(timeStamp).digest('hex');
