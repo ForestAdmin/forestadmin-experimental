09bd0ff36e97ab27730c3a9c82d5d7a1889d7452
Arnaud Moncel <arnaud.moncel43@gmail.com>
2025-04-02 16:13:53 +0200

    feat: restart on sse lost connection

--- packages/datasource-rpc/src/index.ts
+++ packages/datasource-rpc/src/index.ts
@@ -1,70 +1,79 @@
-import { CollectionCustomizer } from '@forestadmin/datasource-customizer';
-import { BaseDataSource, DataSourceFactory, Logger } from '@forestadmin/datasource-toolkit';
+import { DataSourceFactory, Logger } from '@forestadmin/datasource-toolkit';
+import crypto from 'crypto';
+import { EventSource } from 'eventsource';
 import superagent from 'superagent';
 
 import RpcDataSource from './datasource';
-import { RpcDataSourceOptions } from './types';
-import { appendHeaders } from './utils';
+import { RpcDataSourceOptions, RpcSchema } from './types';
+import { appendHeaders, getAuthoriztionHeaders } from './utils';
 
-export function createRpcDataSource(options: RpcDataSourceOptions): DataSourceFactory {
-  return async (logger: Logger) => {
-    const { authSecret, uri } = options;
+export { reconciliateRpc } from './plugins';
 
-    logger('Info', `Getting schema from Rpc agent on ${uri}.`);
+async function getintrospection(logger: Logger, uri: string, authSecret: string) {
+  logger('Info', `Getting schema from Rpc agent on ${uri}.`);
 
-    const introRq = superagent.get(`${uri}/forest/rpc-schema`);
-    appendHeaders(introRq, authSecret);
+  const introRq = superagent.get(`${uri}/forest/rpc-schema`);
+  appendHeaders(introRq, authSecret);
 
-    const introResp = await introRq.send();
+  const introResp = await introRq.send();
 
-    const introspection = introResp.body;
+  return introResp.body;
+}
 
-    return new RpcDataSource(logger, options, introspection);
+function getHash(schema: RpcSchema) {
+  return crypto.createHash('sha1').update(JSON.stringify(schema)).digest('hex');
+}
+
+function runRecon(
+  logger: Logger,
+  uri: string,
+  authSecret: string,
+  originalHash: string,
+  restartAgent: () => Promise<void>,
+) {
+  const es = new EventSource(`${uri}/forest/sse`, {
+    fetch: (input, init) => {
+      return fetch(input, {
+        ...init,
+        headers: {
+          ...init.headers,
+          ...getAuthoriztionHeaders(authSecret),
+        },
+      });
+    },
+  });
+
+  let reconnecting = false;
+
+  es.onerror = error => {
+    reconnecting = true;
+    logger('Debug', `SSE error: ${error.message}`);
+  };
+
+  es.onopen = async () => {
+    if (reconnecting) {
+      logger('Info', `Reconnecting with RPC agent on ${uri}.`);
+      const newIntrospection = await getintrospection(logger, uri, authSecret);
+      const newHash = getHash(newIntrospection);
+
+      if (originalHash !== newHash) {
+        logger('Info', `Schema of RPC agent on ${uri} change: restarting.`);
+        await restartAgent();
+        es.close();
+      }
+    }
   };
 }
 
-export function generateRpcRelations(dataSource) {
-  dataSource.compositeDataSource.dataSources.forEach((d: BaseDataSource) => {
-    if (d instanceof RpcDataSource) {
-      Object.entries(d.rpcRelations).forEach(([name, relations]) => {
-        const cz: CollectionCustomizer = dataSource.getCollection(name);
+export function createRpcDataSource(options: RpcDataSourceOptions): DataSourceFactory {
+  return async (logger: Logger, restartAgent: () => Promise<void>) => {
+    const { authSecret, uri } = options;
 
-        Object.entries(relations).forEach(([relationName, relationDefinition]) => {
-          switch (relationDefinition.type) {
-            case 'ManyToMany':
-              cz.addManyToManyRelation(
-                relationName,
-                relationDefinition.foreignCollection,
-                relationDefinition.throughCollection,
-                {
-                  foreignKey: relationDefinition.foreignKey,
-                  foreignKeyTarget: relationDefinition.foreignKeyTarget,
-                  originKey: relationDefinition.originKey,
-                  originKeyTarget: relationDefinition.originKeyTarget,
-                },
-              );
-              break;
-            case 'OneToMany':
-              cz.addOneToManyRelation(relationName, relationDefinition.foreignCollection, {
-                originKey: relationDefinition.originKey,
-                originKeyTarget: relationDefinition.originKeyTarget,
-              });
-              break;
-            case 'OneToOne':
-              cz.addOneToOneRelation(relationName, relationDefinition.foreignCollection, {
-                originKey: relationDefinition.originKey,
-                originKeyTarget: relationDefinition.originKeyTarget,
-              });
-              break;
-            default:
-              cz.addManyToOneRelation(relationName, relationDefinition.foreignCollection, {
-                foreignKey: relationDefinition.foreignKey,
-                foreignKeyTarget: relationDefinition.foreignKeyTarget,
-              });
-              break;
-          }
-        });
-      });
-    }
-  });
+    const introspection = await getintrospection(logger, uri, authSecret);
+    const originalHash = getHash(introspection);
+
+    runRecon(logger, uri, authSecret, originalHash, restartAgent);
+
+    return new RpcDataSource(logger, options, introspection);
+  };
 }
