b3732f61a99b88c2d173f1947e10179739f498c6
alban bertolini <albanb@forestadmin.com>
2025-06-11 17:28:01 +0200

    fix: code

--- packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-color-picker.ts
+++ packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-color-picker.ts
@@ -1,7 +1,7 @@
 import ActionField from './action-field';
 
 export default class ActionFieldColorPicker<TypingsSchema> extends ActionField<TypingsSchema> {
-  async fill(value: number | string) {
+  async fill(value?: number | string) {
     await this.fieldsFormStates.setFieldValue(
       this.name,
       this.isValueUndefinedOrNull(value) ? value : value.toString(),
--- packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-string.ts
+++ packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-string.ts
@@ -1,7 +1,10 @@
 import ActionField from './action-field';
 
 export default class ActionFieldString<TypingsSchema> extends ActionField<TypingsSchema> {
-  async fill(value: number | string) {
-    await this.fieldsFormStates.setFieldValue(this.name, value.toString());
+  async fill(value?: number | string) {
+    await this.fieldsFormStates.setFieldValue(
+      this.name,
+      this.isValueUndefinedOrNull(value) ? value : value.toString(),
+    );
   }
 }
