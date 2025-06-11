0230844246d2d64db81190f718a50cf3645f7dad
scra <albanbertolini@gmail.com>
2025-06-11 16:31:20 +0200

    fix(agent-testing): allow null or undefined values on fill helper (#142)

--- packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-color-picker.ts
+++ packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-color-picker.ts
@@ -2,7 +2,10 @@ import ActionField from './action-field';
 
 export default class ActionFieldColorPicker<TypingsSchema> extends ActionField<TypingsSchema> {
   async fill(value: number | string) {
-    await this.fieldsFormStates.setFieldValue(this.name, value.toString());
+    await this.fieldsFormStates.setFieldValue(
+      this.name,
+      this.isValueUndefinedOrNull(value) ? value : value.toString(),
+    );
   }
 
   async isOpacityEnable() {
--- packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-number.ts
+++ packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-number.ts
@@ -1,7 +1,10 @@
 import ActionField from './action-field';
 
 export default class ActionFieldNumber<TypingsSchema> extends ActionField<TypingsSchema> {
-  async fill(value: number | string) {
-    await this.fieldsFormStates.setFieldValue(this.name, Number(value));
+  async fill(value?: number | string) {
+    await this.fieldsFormStates.setFieldValue(
+      this.name,
+      this.isValueUndefinedOrNull(value) ? value : Number(value),
+    );
   }
 }
