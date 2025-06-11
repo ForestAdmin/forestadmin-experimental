fdf7bc76401a48380a9d7a94e74934a7b6a6648e
scra <albanbertolini@gmail.com>
2025-06-11 17:31:14 +0200

    fix(agent-testing): fill helper on action form (#143)

--- packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-json.ts
+++ packages/agent-nodejs-testing/src/remote-agent-client/action-fields/action-field-json.ts
@@ -1,7 +1,7 @@
 import ActionField from './action-field';
 
 export default class ActionFieldJson<TypingsSchema> extends ActionField<TypingsSchema> {
-  async fill(value: object) {
+  async fill(value?: object) {
     await this.fieldsFormStates.setFieldValue(this.name, value);
   }
 }
