b3aa75d7f93073c70e60572736d28508725982a2
Arnaud Moncel <arnaud.moncel43@gmail.com>
2026-01-09 11:59:12 +0100

    fix(rpc): handle rename collection

--- packages/datasource-rpc/src/plugins.ts
+++ packages/datasource-rpc/src/plugins.ts
@@ -1,54 +1,74 @@
+import type { PluginOptions } from './types';
+
 import { CollectionCustomizer } from '@forestadmin/datasource-customizer';
-import { BaseDataSource } from '@forestadmin/datasource-toolkit';
+import { BaseDataSource, DataSourceDecorator } from '@forestadmin/datasource-toolkit';
 
 import RpcDataSource from './datasource';
 
+function getRealDatasource(datasource) {
+  let d = datasource;
+
+  while (datasource instanceof DataSourceDecorator) {
+    // @ts-ignore
+    d = datasource.childDataSource;
+  }
+
+  return d;
+}
+
+function getCollectionName(collectionName: string, rename?: PluginOptions['rename']) {
+  if (!rename) return collectionName;
+
+  return typeof rename === 'function' ? rename(collectionName) : rename[collectionName];
+}
+
 // eslint-disable-next-line import/prefer-default-export
-export function reconciliateRpc(dataSource) {
-  dataSource.compositeDataSource.dataSources.forEach((d: BaseDataSource) => {
+export function reconciliateRpc(dz, options?: PluginOptions) {
+  dz.compositeDataSource.dataSources.forEach((datasource: BaseDataSource | DataSourceDecorator) => {
+    const d = getRealDatasource(datasource);
+
     if (d instanceof RpcDataSource) {
       d.collections.forEach(c => {
         if (!c.schema.searchable) {
-          const cz: CollectionCustomizer = dataSource.getCollection(c.name);
+          const cz: CollectionCustomizer = dz.getCollection(
+            getCollectionName(c.name, options.rename),
+          );
           cz.disableSearch();
         }
       });
 
       Object.entries(d.rpcRelations).forEach(([name, relations]) => {
-        const cz: CollectionCustomizer = dataSource.getCollection(name);
+        const cz: CollectionCustomizer = dz.getCollection(name);
 
         Object.entries(relations).forEach(([relationName, relationDefinition]) => {
+          const foreignCollection = getCollectionName(
+            relationDefinition.foreignCollection,
+            options.rename,
+          );
+
           switch (relationDefinition.type) {
-            case 'ManyToMany':
+            case 'ManyToMany': {
+              const throughCollection = getCollectionName(
+                relationDefinition.throughCollection,
+                options.rename,
+              );
               cz.addManyToManyRelation(
                 relationName,
-                relationDefinition.foreignCollection,
-                relationDefinition.throughCollection,
-                {
-                  foreignKey: relationDefinition.foreignKey,
-                  foreignKeyTarget: relationDefinition.foreignKeyTarget,
-                  originKey: relationDefinition.originKey,
-                  originKeyTarget: relationDefinition.originKeyTarget,
-                },
+                foreignCollection,
+                throughCollection,
+                relationDefinition,
               );
               break;
+            }
+
             case 'OneToMany':
-              cz.addOneToManyRelation(relationName, relationDefinition.foreignCollection, {
-                originKey: relationDefinition.originKey,
-                originKeyTarget: relationDefinition.originKeyTarget,
-              });
+              cz.addOneToManyRelation(relationName, foreignCollection, relationDefinition);
               break;
             case 'OneToOne':
-              cz.addOneToOneRelation(relationName, relationDefinition.foreignCollection, {
-                originKey: relationDefinition.originKey,
-                originKeyTarget: relationDefinition.originKeyTarget,
-              });
+              cz.addOneToOneRelation(relationName, foreignCollection, relationDefinition);
               break;
             default:
-              cz.addManyToOneRelation(relationName, relationDefinition.foreignCollection, {
-                foreignKey: relationDefinition.foreignKey,
-                foreignKeyTarget: relationDefinition.foreignKeyTarget,
-              });
+              cz.addManyToOneRelation(relationName, foreignCollection, relationDefinition);
               break;
           }
         });
