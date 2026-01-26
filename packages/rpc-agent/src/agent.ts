6372499bd80f67d847389743454d1d5e4252ef7f
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2026-01-26 16:40:30 +0100

    feat(rpc agent): add mark collection as rpc option on add datasource (#195)

--- packages/rpc-agent/src/agent.ts
+++ packages/rpc-agent/src/agent.ts
@@ -1,15 +1,46 @@
-import { Agent } from '@forestadmin/agent';
+import { Agent, AgentOptions } from '@forestadmin/agent';
 import { ForestAdminHttpDriverServices } from '@forestadmin/agent/dist/services';
-import { TCollectionName, TSchema } from '@forestadmin/datasource-customizer';
-import { DataSource } from '@forestadmin/datasource-toolkit';
+import { DataSourceOptions, TCollectionName, TSchema } from '@forestadmin/datasource-customizer';
+import { DataSource, DataSourceFactory } from '@forestadmin/datasource-toolkit';
 
+import RpcDataSourceCustomizer from './datasource-customizer';
 import { makeRpcRoutes } from './routes';
 import SseRoute from './routes/sse';
 
 export default class RpcAgent<S extends TSchema = TSchema> extends Agent<S> {
   private readonly rpcCollections: string[] = [];
+  protected override customizer: RpcDataSourceCustomizer<S>;
 
   sseRoute: SseRoute;
+
+  constructor(options: AgentOptions) {
+    super(options);
+
+    this.customizer = new RpcDataSourceCustomizer<S>({
+      ignoreMissingSchemaElementErrors: options.ignoreMissingSchemaElementErrors || false,
+    });
+  }
+
+  override addDataSource(
+    factory: DataSourceFactory,
+    options?: DataSourceOptions & { markCollectionsAsRpc?: boolean },
+  ) {
+    let markCollectionsCallback = null;
+
+    if (options?.markCollectionsAsRpc) {
+      markCollectionsCallback = (datasource: DataSource) => {
+        datasource.collections.forEach(c => this.rpcCollections.push(c.name));
+      };
+    }
+
+    this.customizer.addDataSource(
+      factory,
+      { ...options, markCollectionsCallback },
+      this.restart.bind(this),
+    );
+
+    return this;
+  }
 
   override getRoutes(dataSource: DataSource, services: ForestAdminHttpDriverServices) {
     const routes = makeRpcRoutes(dataSource, this.options, services, this.rpcCollections);
---
+++ packages/rpc-agent/src/datasource-customizer.ts
@@ -0,0 +1,42 @@
+import {
+  DataSourceCustomizer,
+  DataSourceOptions,
+  TSchema,
+} from '@forestadmin/datasource-customizer';
+import PublicationDataSourceDecorator from '@forestadmin/datasource-customizer/dist/decorators/publication/datasource';
+import RenameCollectionDataSourceDecorator from '@forestadmin/datasource-customizer/dist/decorators/rename-collection/datasource';
+import { DataSource, DataSourceFactory } from '@forestadmin/datasource-toolkit';
+
+export default class RpcDataSourceCustomizer<
+  S extends TSchema = TSchema,
+> extends DataSourceCustomizer<S> {
+  override addDataSource(
+    factory: DataSourceFactory,
+    options?: DataSourceOptions & { markCollectionsCallback?: (datasource: DataSource) => void },
+    restartAgentFunction?: () => Promise<void>,
+  ): this {
+    this.stack.queueCustomization(async logger => {
+      let dataSource = await factory(logger, restartAgentFunction);
+
+      if (options?.include || options?.exclude) {
+        const publicationDecorator = new PublicationDataSourceDecorator(dataSource, logger);
+        publicationDecorator.keepCollectionsMatching(options.include, options.exclude);
+        dataSource = publicationDecorator;
+      }
+
+      if (options?.rename) {
+        const renamedDecorator = new RenameCollectionDataSourceDecorator(dataSource);
+        renamedDecorator.renameCollections(options.rename);
+        dataSource = renamedDecorator;
+      }
+
+      if (options.markCollectionsCallback) {
+        options.markCollectionsCallback(dataSource);
+      }
+
+      this.compositeDataSource.addDataSource(dataSource);
+    });
+
+    return this;
+  }
+}
