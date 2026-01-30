7ea909c09f89a6e244668d267adb1a0c142daeae
scra <albanbertolini@gmail.com>
2026-01-30 15:54:12 +0100

    chore(agent-nodejs-testing): deprecate in favor of @forestadmin/agent-testing (#201)

--- packages/agent-nodejs-testing/src/index.ts
+++ packages/agent-nodejs-testing/src/index.ts
@@ -1,3 +1,9 @@
+console.warn(
+  '[@forestadmin-experimental/agent-nodejs-testing] This package is deprecated. ' +
+    'Please migrate to @forestadmin/agent-testing. ' +
+    'See: https://www.npmjs.com/package/@forestadmin/agent-testing',
+);
+
 export * from './integrations';
 export * from './units';
 export * from './remote-agent-client';
